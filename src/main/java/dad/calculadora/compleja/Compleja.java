9d4b237ab8a91224cf896cb8acb52c538d78b4b1
SamirElKharrat <samirelkharrat@gmail.com>
2020-10-13 10:45:02 +0100

    Comenzamos el proyecto

---
+++ src/main/java/dad/calculadora/compleja/Compleja.java
@@ -0,0 +1,15 @@
+package dad.calculadora.compleja;
+
+import com.sun.glass.ui.Application;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class Compleja {
+	private DoubleProperty real = new SimpleDoubleProperty(0);
+	private DoubleProperty imaginario = new SimpleDoubleProperty(0);
+	
+	
+
+
+}
---
+++ src/main/java/dad/calculadora/compleja/Main.java
@@ -0,0 +1,10 @@
+package dad.calculadora.compleja;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
